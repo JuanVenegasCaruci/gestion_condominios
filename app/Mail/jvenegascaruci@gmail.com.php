ff0453d9aa85cfb15760deedb2f49541f31649a9
JuanVenegasCaruci <jvenegascaruci@gmail.com>
2025-01-21 19:50:35 -0400

    Actualizacion martes 22 de enero 2025.

---
+++ app/Mail/jvenegascaruci@gmail.com.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class jvenegascaruci@gmail.com extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.jvenegascaruci@gmail.com')
+                    ->from('jvenegascaruci@gmail.com', 'Juan C. Venegas')
+                    ->subject('Recibo de Pago');
+    }
+}
